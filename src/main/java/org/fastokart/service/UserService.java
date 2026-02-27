913659e3fd7b0d23bdacc4b17b90cfcae6d60863
Naz Mul <nazmul@example.com>
2026-02-27 11:13:06 +0530

    feat(user): added login functionality

--- src/main/java/org/fastokart/service/UserService.java
+++ src/main/java/org/fastokart/service/UserService.java
@@ -1,8 +1,10 @@
 package org.fastokart.service;
 
+import org.fastokart.dto.UserLoginDTO;
 import org.fastokart.dto.UserRegisterDTO;
 
 public interface UserService {
     // register new user
     void register(UserRegisterDTO dto);
+    String login(UserLoginDTO dto);
 }
